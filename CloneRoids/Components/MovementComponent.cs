c831e20531c0f808a70690270a88baefba9c7048
Pietro Carrara <piticarrara@gmail.com>
2017-05-25 01:35:02 +0000

    Adiconar componentes de movimentação
    "MovementComponent" gerencia o movimento da entidade
    "RotationFixer" corrige o giro da entidade

---
+++ CloneRoids/Components/MovementComponent.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Nez;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
+
+namespace CloneRoids.Components
+{
+    class MovementComponent : Component, IUpdatable
+    {
+        public int Speed, TurnSpeed;
+
+        public MovementComponent(int speed = 0, int turnSpeed = 0)
+        {
+            Speed = speed;
+            TurnSpeed = turnSpeed;
+        }
+
+        public void update()
+        {
+            var movement = Vector2.Zero;
+            float rotation = 0;
+            int velocity = 0;
+
+            // Giro
+            if (Input.isKeyDown(Keys.Right))
+                rotation = TurnSpeed;
+            else if (Input.isKeyDown(Keys.Left))
+                rotation = -TurnSpeed;
+
+            // Aplica o giro (em radianos), e armazena
+            // a rotação na variável rotation
+            rotation = entity.transform.rotation += rotation * Time.deltaTime;
+
+            // Pra frente, pra traz
+            if (Input.isKeyDown(Keys.Down))
+            {
+                velocity = Speed;
+            }
+            else if (Input.isKeyDown(Keys.Up))
+            {
+                velocity = -Speed;
+            }
+
+            // Calcula o movimento para X e Y
+            movement.X = - velocity * Mathf.sin(rotation);
+            movement.Y = velocity * Mathf.cos(rotation);
+
+            // Aplica o movimento
+            entity.move(movement * Time.deltaTime);
+        }
+    }
+}
---
+++ CloneRoids/Components/RotationFixer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Nez;
+
+namespace CloneRoids.Components
+{
+    class RotationFixer : Component, IUpdatable
+    {
+        public void update()
+        {
+            while (entity.transform.rotationDegrees > 360)
+                entity.transform.rotationDegrees -= 360;
+
+            while (entity.transform.rotationDegrees < 0)
+                entity.transform.rotationDegrees += 360;
+        }
+    }
+}
