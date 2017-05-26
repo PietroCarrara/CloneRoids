76469829c42453810c451aa6d6232ae41597b578
Arthur Rosso <arthurderosso@gmail.com>
2017-05-26 18:24:00 -0300

    Adiciona lógica de game over

---
+++ CloneRoids/Components/AsteroidCollider.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Nez;
+using CloneRoids.Scenes;
+
+namespace CloneRoids.Components
+{
+    public class AsteroidCollider : Component, IUpdatable
+    {
+        Collider coll;
+
+        public override void onAddedToEntity()
+        {
+            base.onAddedToEntity();
+
+            coll = entity.getCollider<Collider>();
+        }
+
+        public void update()
+        {
+            var cena = entity.scene as MainScene;
+
+            foreach (var asteroid in cena.Asteroides)
+            {
+                var result = new CollisionResult();
+                if (coll.collidesWith(asteroid.getCollider<Collider>(), out result))
+                {
+                    entity.destroy();
+                    return;
+                }
+            }
+        }
+    }
+}
